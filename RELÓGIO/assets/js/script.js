57976f753b9b61a58221dd622f2a2229c1e64753
Erick Serra <110630683+lovelyyulian@users.noreply.github.com>
2024-01-07 18:39:45 -0300

    Add files via upload

---
+++ RELÓGIO/assets/js/script.js
@@ -0,0 +1,19 @@
+alert("Hello there,\nI just wrote this clock script by watching Larissa Kich\non her YouTube channel. She owns any rights from this.\nAnd I had a lot of fun when coding.\n\nEnjoy this amazing clock!\n- Erick Serra")
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+});
